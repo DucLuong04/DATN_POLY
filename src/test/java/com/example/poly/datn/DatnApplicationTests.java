4892738c0bee89b06d0b4bc2559170b63870dfc9
DucLuong04 <luongthptnqb123@gmail.com>
2024-10-20 20:27:12 +0700

    init

---
+++ src/test/java/com/example/poly/datn/DatnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poly.datn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
